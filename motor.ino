ecbf8a2b4a6a981ffe3be64fc4de36994f4b04fb
Andrew Jacobson <ajacobson@aapl000794-2.local>
2016-09-28 13:42:22 -0400

    motor is working

---
+++ motor.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode;(11, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+analogRead(11);
+analogWrite(11, 150);
+}
