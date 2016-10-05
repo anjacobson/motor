7b1ee023adf5d28a9fdbe25e91681c2dca53db00
Andrew Jacobson <ajacobson@aapl000619.local>
2016-10-05 14:50:21 -0400

    button added but not working

--- motor.ino
+++ motor.ino
@@ -1,10 +1,17 @@
 void setup() {
   // put your setup code here, to run once:
 pinMode;(11, OUTPUT);
+pinMode;(8, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-analogRead(11);
-analogWrite(11, 150);
+if (digitalRead(8)== 1 ) {
+  analogWrite(11, 150);
+  }
+
 }
+
+
+
+
